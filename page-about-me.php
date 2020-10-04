749bbc6ad0bd27f38cc237ab4eb8f98e5c76d7a3
Monika Kwiatkowska <mokwiatkowska@gmail.com>
2020-10-04 14:42:44 +0200

    add about me template

---
+++ page-about-me.php
@@ -0,0 +1,23 @@
+*/
+Template: About me
+*/
+
+<?php get_header(); ?>
+<?php
+if ( have_posts() ):
+while( have_posts() ): the_post(); ?> 
+
+<div> About me page </div>
+<p><?php the_content(); ?></p>
+
+<hr>
+<?php
+    endwhile;
+endif; 
+
+?>
+
+
+
+<?php get_footer(); ?>
+
