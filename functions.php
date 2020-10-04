dce0efc7dcd37396cad8fe8bd7f2ad4fc48c0805
Monika Kwiatkowska <mokwiatkowska@gmail.com>
2020-10-04 14:36:26 +0200

    initial commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+	
+function penguin_script_enqueue() {
+	
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/penguin.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/penguin.js', array(), '1.0.0', true);
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'penguin_script_enqueue');
+
+function penguin_theme_setup() {
+	
+	register_nav_menu('primary', 'Primary Header Navigation');
+	register_nav_menu('secondary', 'Footer Navigation');
+	
+}
+
+add_action('init', 'penguin_theme_setup');
---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Penguin Theme</title>
+		<?php wp_head(); ?>
+	</head>
+	
+	<body <?php body_class(); ?>>
+		
+		<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
